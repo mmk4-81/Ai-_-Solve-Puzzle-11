bdb0a517097fc0bc882601df620efdcaba345827
mmk <mmkkarbalaei@gmail.com>
2024-02-09 23:30:08 +0330

    create solvePuzzle_bfs

--- solvePuzzle.cpp
+++ solvePuzzle.cpp
@@ -10,11 +10,11 @@
 using namespace std;
 
 void printMatrix(string matrix);
-
-
-int main(){
+void solvePuzzle_bfs(string input, string goal); 
+int main()
+{
     string input = "6o485b12793a"; //  o23617284754 27o617384524 6o485b12793a
-    
+
     auto start = chrono::high_resolution_clock::now();
 
     cout << "\nInput characters: " << input << endl;
@@ -26,18 +26,31 @@ int main(){
     cout << "Sorted characters:" << endl;
     printMatrix(goal);
 
-    cout << "----------------------------------------------------------------------------" << endl << endl;
+    cout << "----------------------------------------------------------------------------" << endl
+    << endl;
+    solvePuzzle_bfs(input, goal);
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Total execution time: " << elapsed.count() << " seconds" << endl;
+
     return 0;
 }
 
-void printMatrix(string matrix) {
+void printMatrix(string matrix)
+{
     const int Rows = 3;
     const int Cols = 4;
-    for (int i = 0; i < Rows; i++) {
-        for (int j = 0; j < Cols; j++) {
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
             int index = i * Cols + j;
             cout << matrix[index] << ' ';
         }
         cout << endl;
     }
+}
+
+void solvePuzzle_bfs(string input, string goal) {
+    
 }
