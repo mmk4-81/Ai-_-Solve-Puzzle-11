0bf2d03ec78a960f2a90a432d4b1c774ddbbc745
mmk <mmkkarbalaei@gmail.com>
2024-02-09 23:33:30 +0330

    solve puzzle with bfs search

--- solvePuzzle.cpp
+++ solvePuzzle.cpp
@@ -9,7 +9,8 @@
 
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     string data;
     shared_ptr<Node> parent;
@@ -20,7 +21,13 @@ public:
 };
 
 void printMatrix(string matrix);
-void solvePuzzle_bfs(string input, string goal); 
+void solvePuzzle_bfs(string input, string goal);
+string swapCharacters(string L, int index, int n) {
+    string result = L;
+    swap(result[index], result[index + n]);
+    return result;
+}
+
 int main()
 {
     string input = "6o485b12793a"; //  o23617284754 27o617384524 6o485b12793a
@@ -37,7 +44,7 @@ int main()
     printMatrix(goal);
 
     cout << "----------------------------------------------------------------------------" << endl
-    << endl;
+         << endl;
     solvePuzzle_bfs(input, goal);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end - start;
@@ -61,6 +68,47 @@ void printMatrix(string matrix)
     }
 }
 
-void solvePuzzle_bfs(string input, string goal) {
+void solvePuzzle_bfs(string input, string goal)
+{
+    queue<shared_ptr<Node>> frontier;
+    unordered_set<string> explored;
 
+    frontier.push(make_shared<Node>(input, nullptr, "initial_state", 0));
+
+    while (!frontier.empty())
+    {
+
+        shared_ptr<Node> L = frontier.front();
+        frontier.pop();
+
+     
+
+        int indexBlank = L->data.find('o');
+
+        int moves[] = {-1, 1, -4, 4};
+
+        for (int i = 0; i < 4; i++)
+        {
+            int move = moves[i];
+            int newIndex = indexBlank + move;
+
+            if (newIndex >= 0 && newIndex < L->data.size() && (newIndex / 4 == indexBlank / 4 || newIndex % 4 == indexBlank % 4))
+            {
+
+                string newState = swapCharacters(L->data, indexBlank, move);
+
+                if (explored.find(newState) == explored.end())
+                {
+
+                    string moveDirection = (move == -1) ? "Left" : (move == 1) ? "Right"
+                                                               : (move == -4)  ? "Up"
+                                                                               : "Down";
+                    shared_ptr<Node> state = make_shared<Node>(newState, L, "Move " + moveDirection, L->level + 1);
+                    frontier.push(state);
+                    explored.insert(newState);
+                }
+            }
+        }
+    }
+    cout << "No solution found!" << endl;
 }
